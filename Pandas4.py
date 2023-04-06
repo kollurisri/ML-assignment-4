7b9520d515bf5d486395658595170dcd7617a7c5
kollurisri <122470948+kollurisri@users.noreply.github.com>
2023-04-05 23:07:28 -0500

    Aggregate the data using: min, max, count, mean.

---
+++ Pandas4.py
@@ -0,0 +1,2 @@
+df.agg({'Pulse' : ['min', 'max', 'count', 'mean'], 'Maxpulse' : ['min', 'max', 'count', 'mean'], 
+        'Calories' : ['min', 'max', 'count', 'mean'] }) #agg method to aggregate operation
